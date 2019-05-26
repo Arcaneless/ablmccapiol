69a36e768594a0d17426dacb02f5ae459bfd1f80
iMac <kings88819@gamil.com>
2019-05-26 20:39:49 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import ablmccapi from './ablmccapi'
 var express = require('express')
 const app = express()
 const ABLMCCAPI = new ablmccapi()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => res.send('Hello World :)'))
 
